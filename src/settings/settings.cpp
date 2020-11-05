3ec5b97519ef8ae3c77cf39e3b97e1cb03b79052
Hilda Palva <hilda.palva@aalto.fi>
2020-11-05 15:59:06 +0200

    settingsclass with playername

---
+++ src/settings/settings.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+
+class settings
+{
+public:
+
+    int setName(std::string name)
+    {
+        PlayerName = name;
+        std::string turha;
+        std::ofstream file;
+        file.open("settings.txt");
+        file << "PlayerName:" << name << std::endl;
+        file.close();
+        return 0;
+    }
+
+    std::string getName()
+    {
+        std::string name;
+        std::string turha = "a";
+        std::ifstream file;
+        file.open("settings.txt");
+        while (!file.eof() && turha != "PlayerName")
+        {
+            std::getline(file, turha, ':');
+            if (turha == "PlayerName")
+            {
+                std::getline(file, name, '\n');
+            }
+        }
+        file.close();
+        return name;
+    }
+private:
+    std::string PlayerName;
+};
