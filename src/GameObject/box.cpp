06854fc88ca3ccbe7bd208cfd5fec81c86465b99
Hilda Palva <hilda.palva@aalto.fi>
2020-11-19 17:50:04 +0200

    box not shaking like crazy

--- src/GameObject/box.cpp
+++ src/GameObject/box.cpp
@@ -4,7 +4,7 @@
 Box::Box(sf::Int32 id, std::string spritePath, b2World *world): DynamicObject(id, spritePath, world) 
 {
     b2PolygonShape pShape;
-    pShape.SetAsBox(1,1);
+    pShape.SetAsBox(0.5,0.5);
     shape_ = pShape;
     b2FixtureDef fDef;
     fDef.shape = &shape_;
@@ -20,8 +20,8 @@ Box::~Box() {
 
 void Box::PrivateUpdate(float dt)
 {
-    std::cout << GetTransform().p.x << " " << GetTransform().p.y << " Olen taalla" << std::endl;
-    body_->ApplyForceToCenter(15 * frictionMultiplier_ * -GetVelocity(), true);
-    body_->ApplyTorque(15 * frictionMultiplier_ * -GetAngularVelocity(), true);
+    //std::cout << GetTransform().p.x << " " << GetTransform().p.y << " Olen taalla" << std::endl;
+    body_->ApplyForceToCenter(8 * frictionMultiplier_ * -GetVelocity(), true);
+    body_->ApplyTorque(5 * frictionMultiplier_ * -GetAngularVelocity(), true);
 }
 
